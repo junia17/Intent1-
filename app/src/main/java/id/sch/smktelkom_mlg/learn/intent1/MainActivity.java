a68bb62911c2fd51d6878f655aafd681a2df09b1
Junior <jhimmayati17@gmail.com>
2016-10-31 17:51:25 +0700

    startActivity Part 2.

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -5,19 +5,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NAMA = "Nama";
+    public static final String UMUR = "Umur";
+    EditText etNama;
+    EditText etUmur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etUmur = (EditText) findViewById(R.id.editTextUmur);
+
+        setTitle("Input");
+
         findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivities(new Intent(MainActivity.this, HasilActivity.class));
+                goHasil();
+
             }
         });
+    }
+
+    private void goHasil() {
+        String nama = etNama.getText().toString();
+        String umurStr = etUmur.getText().toString();
+
+        int umur = umurStr.isEmpty() ? 0 : Integer.parseInt(umurStr);
+
+        Intent intent = new Intent(MainActivity.this, HasilActivity.class);
+        Intent.putExtra(NAMA, nama);
+        Intent.putExtra(UMUR, umur);
+
+        startActivity(intent);
     }
 }
